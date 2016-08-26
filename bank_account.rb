e084140ac6cc43e6a5c117de416d6049537dfc00
Olivia <reach.olivialegge@gmail.com>
2016-08-26 15:10:20 -0700

    Wave 3 refactor, cleaned up withdraw methods

--- bank_account.rb
+++ bank_account.rb
@@ -1,7 +1,6 @@
-#utilize charge_fee method
-#Change to cents
 require 'csv'
 require 'time'
+
 
 module Bank
 
@@ -11,21 +10,21 @@ module Bank
 
     def initialize(id,balance,open_date)
       @min_balance = 0
-      @low_fund_warning = "Insufficient fund warning: Balance cannot be less than $#{@min_balance}"
       @id = id
       if under_min?(balance)
-        raise ArgumentError.new(@low_fund_warning)
+        raise ArgumentError.new(warning)
       end
       @balance = balance
       @open_date = open_date
       @fee = 0
     end
 
-    def withdraw(amount)
-      if under_min?(balance - (amount + @fee))
-        puts @low_fund_warning
+    def withdraw(amount,fee = @fee, min = @min_balance)
+      if under_min?(balance - (amount + fee), min)
+        puts warning(min)
+        return @balance
       else
-        @balance = charge_fee(@fee) - amount
+        return @balance = charge_fee(fee) - amount
       end
     end
 
@@ -37,13 +36,16 @@ module Bank
       return @balance - (fee)
     end
 
+    def warning(min = @min_balance)
+        puts "Insufficient fund warning: Balance cannot be less than $#{min}"
+    end
 
     def add_owner(owner)
       @owner = owner
     end
 
-    def under_min?(amount)
-      amount < @min_balance
+    def under_min?(amount, min = @min_balance)
+      amount < min
     end
 
     def self.all
@@ -69,7 +71,11 @@ module Bank
       end
     end
 
-end
+    # def self.money_to_dollars(cents)
+    #   return "$#{cents.to_f/100.00}"
+    # end
+
+  end
 
 class SavingsAccount < Account
 
@@ -77,17 +83,15 @@ class SavingsAccount < Account
   attr_accessor :fee
 
   def initialize(id,balance,open_date)
-    @min_balance = 1000
     super
-    if under_min?(balance)
-      raise ArgumentError.new(low_fund_warning)
-    end
     @fee = 200
+    @min_balance = 1000
   end
 
   def add_interest(rate)
     interest_earned = balance * rate/10000
   end
+
 end
 
 class CheckingAccount < Account
@@ -97,35 +101,24 @@ class CheckingAccount < Account
   def initialize(id,balance,open_date)
     super
     @check_count = 0
-  end
-
-  def withdraw(amount)
-    fee = 100
-    if under_min?(balance - (amount + fee))
-      puts @low_fund_warning
-    else
-      @balance = charge_fee(fee) - amount
-    end
-
+    @fee = 100
   end
 
   def withdraw_using_check(amount)
-    check_fee = 200
-    if @balance - (check_fee + amount) > -1000
-      if @check_count <= 3
-         @check_count += 1
-         return @balance - (amount + check_fee)
-      else
-        @check_count += 1
-        return @balance -= (amount + check_fee)
-      end
+    @check_fee = 200
+    @min = -1000
+    if @check_count <= 3
+      @check_count += 1
+      withdraw(amount,0,@min)
     else
-      puts "Insufficient fund warning: Balance cannot be less than -$10."
+      @check_count +=1
+      withdraw(amount,@check_fee,@min)
     end
   end
 
+
   def reset_checks
-    @check = 0
+    @check_count = 0
   end
 
 end
@@ -141,43 +134,3 @@ end
 
   end
 end
-
-#________Tests for Wave 1
-account_info = CSV.read('support/accounts.csv')
-id = account_info[0][0].to_i
-balance = account_info[0][1].to_i
-open_date = Time.parse(account_info[0][2])
-
-# tst1 = Bank::Account.new(id,balance,open_date)
-# puts tst1.withdraw(5)
-# puts tst1.balance
-# puts tst1.deposit(5)
-# puts tst1.balance
-# puts tst1.withdraw(5)
-
-#_________Tests for Wave 2
-# Bank::Account.all
-# puts Bank::Account.find(1212)
-
-#_________Tests for Wave 3
-tst2 = Bank::SavingsAccount.new(id,balance,open_date)
-puts tst2.balance
-puts tst2.withdraw(10)
-puts tst2.withdraw(10)
-puts tst2.balance
-
-tst3 = Bank::CheckingAccount.new(id,balance,open_date)
-
-# puts tst3.withdraw_using_check(10)
-# puts tst3.withdraw_using_check(100)
-# puts tst3.withdraw_using_check(10)
-# puts tst3.withdraw_using_check(10)
-# puts tst3.withdraw_using_check(10)
-# puts tst3.withdraw_using_check(10)
-# puts tst3.withdraw_using_check(100000000000)
-
-
-
-# tst3 = Bank::CheckingAccount.new(id,100,open_date)
-# tst3.withdraw(200)
-#
